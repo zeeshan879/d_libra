b97dcd8894f69195bd7f114f04731525b0a52fb1
shakeeb <shakeebanwar250@gmail.com>
2022-03-29 12:54:48 +0500

    done

--- api/settings.py
+++ api/settings.py
@@ -44,11 +44,13 @@ INSTALLED_APPS = [
     'import_export',
     'mptt',
     'taggit',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -165,6 +167,6 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 CORS_ALLOWED_ORIGINS = [
 
     "http://127.0.0.1:3000",
-    
+
     "http://localhost:3000"
 ]
