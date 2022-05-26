c96f30f4e0e589d425e7adfb3d9f6109d4485301
shakeeb <shakeebanwar250@gmail.com>
2022-05-26 18:25:13 +0500

    done

--- api/settings.py
+++ api/settings.py
@@ -183,7 +183,8 @@ CORS_ALLOWED_ORIGINS = [
 
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "https://the-d-libra.netlify.app"
+    "https://the-d-libra.netlify.app",
+    "https://libraa.ml"
 ]
 
 
