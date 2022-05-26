6aab00e2570358b5c07a4f76f82bbe42ef39e8d7
shakeeb <shakeebanwar250@gmail.com>
2022-05-26 16:18:56 +0500

    done

--- api/settings.py
+++ api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','52.15.181.26']
 
@@ -182,6 +182,9 @@ EMAIL_HOST_PASSWORD = config('fromemailpassword')
 CORS_ALLOWED_ORIGINS = [
 
     "http://127.0.0.1:3000",
-
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://the-d-libra.netlify.app"
 ]
+
+
+CSRF_TRUSTED_ORIGINS=["https://api.libraa.ml"]
