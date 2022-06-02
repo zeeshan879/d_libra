9f67189566b346e749cfc0814ebf9dbc4628fb15
shoaib <shoaibbilal101@gmail.com>
2022-06-02 15:15:06 +0500

    Push

--- api/settings.py
+++ api/settings.py
@@ -24,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# Live
 DEBUG = False
+
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','52.15.181.26']
 
