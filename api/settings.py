615c766ffdd3c91097e003dab0168af659ade2c9
shoaib <shoaibbilal101@gmail.com>
2022-06-07 15:27:55 +0500

    Push

--- api/settings.py
+++ api/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
-
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','52.15.181.26']
 
