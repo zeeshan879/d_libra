2821cee1bf72b2cd461625d16e88b1c618ef23c0
shakeeb <shakeebanwar250@gmail.com>
2022-06-07 16:08:39 +0500

    done

--- api/settings.py
+++ api/settings.py
@@ -26,9 +26,6 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # Live
 DEBUG = True
-
-DEBUG = True
-
 ALLOWED_HOSTS = ['localhost','127.0.0.1','52.15.181.26']
 
 
