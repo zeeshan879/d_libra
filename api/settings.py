555d3cd465a40c062437ec378ff179704fe514a1
shakeeb <shakeebanwar250@gmail.com>
2022-09-07 11:57:11 +0500

    deploy

--- api/settings.py
+++ api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # Live
 DEBUG = True
-ALLOWED_HOSTS = ['localhost','127.0.0.1','18.118.37.41','api.libraa.ml']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','18.191.5.228','api.libraa.ml']
 
 
 # Application definition
